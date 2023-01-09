58097c294193daf5234d08dae7002625469ad9da
Joshua Hale <jhale@systemautomation.com>
2023-01-08 22:51:07 -0500

    fun tween effect for UI :)

--- TopPanelGUI.gd
+++ TopPanelGUI.gd
@@ -1,15 +1,20 @@
+# This is for the top panel of the game's UI
+# Currently will turn alpha from 0 -> full when mouse enters area
 extends Panel
 
+@export var fadeTime : float = 0.2
 
 func _ready():
 	self.modulate = "#FFFFFF00"
-	
 
 func _on_mouse_entered():
-	self.modulate = "#FFFFFFFF"
+#NOTE wow these tweens are soo cool!! https://www.youtube.com/watch?v=04TB9gxz-uM
+	var tween = create_tween()
+	tween.set_trans(Tween.TRANS_BOUNCE) # not critical
+	tween.tween_property(self, "modulate", Color("#FFFFFFFF"), fadeTime)
 
 
 func _on_mouse_exited():
-	self.modulate = "#FFFFFF00"
-
+	var tween = create_tween()
+	tween.tween_property(self, "modulate", Color("#FFFFFF00"), fadeTime)
 
