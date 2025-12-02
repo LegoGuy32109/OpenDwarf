d6450f89dc6d65a62c2061358fbf053e0b83e4d3
Josh Hale <joshevanhale@gmail.com>
2025-12-01 22:38:39 -0500

    web-release command added to simplify testing release quality builds

--- static/game/open_dwarf_lib.d.ts
+++ static/game/open_dwarf_lib.d.ts
@@ -8,18 +8,15 @@ export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembl
 export interface InitOutput {
   readonly memory: WebAssembly.Memory;
   readonly main: () => void;
-  readonly wasm_bindgen__convert__closures_____invoke__h1ca557dcc3b60a3b: (a: number, b: number, c: any) => void;
-  readonly wasm_bindgen__closure__destroy__h1e69de07d9687681: (a: number, b: number) => void;
-  readonly wasm_bindgen__closure__destroy__hb913979280c72850: (a: number, b: number) => void;
-  readonly wasm_bindgen__convert__closures_____invoke__h668ab64e39a0d10a: (a: number, b: number, c: any, d: any) => void;
-  readonly wasm_bindgen__convert__closures_____invoke__h7ba3f5c1e6b2bd66: (a: number, b: number) => void;
-  readonly __wbindgen_malloc_command_export: (a: number, b: number) => number;
-  readonly __wbindgen_realloc_command_export: (a: number, b: number, c: number, d: number) => number;
-  readonly __externref_table_alloc_command_export: () => number;
-  readonly __wbindgen_externrefs: WebAssembly.Table;
-  readonly __wbindgen_exn_store_command_export: (a: number) => void;
-  readonly __wbindgen_free_command_export: (a: number, b: number, c: number) => void;
-  readonly __wbindgen_start: () => void;
+  readonly __wasm_bindgen_func_elem_67663: (a: number, b: number, c: number) => void;
+  readonly __wasm_bindgen_func_elem_70889: (a: number, b: number) => void;
+  readonly __wasm_bindgen_func_elem_70890: (a: number, b: number) => void;
+  readonly __wasm_bindgen_func_elem_61985: (a: number, b: number, c: number, d: number) => void;
+  readonly __wasm_bindgen_func_elem_67662: (a: number, b: number) => void;
+  readonly __wbindgen_export: (a: number, b: number) => number;
+  readonly __wbindgen_export2: (a: number, b: number, c: number, d: number) => number;
+  readonly __wbindgen_export3: (a: number) => void;
+  readonly __wbindgen_export4: (a: number, b: number, c: number) => void;
 }
 
 export type SyncInitInput = BufferSource | WebAssembly.Module;
--- static/game/open_dwarf_lib_bg.wasm.d.ts
+++ static/game/open_dwarf_lib_bg.wasm.d.ts
@@ -2,15 +2,12 @@
 /* eslint-disable */
 export const memory: WebAssembly.Memory;
 export const main: () => void;
-export const wasm_bindgen__convert__closures_____invoke__h1ca557dcc3b60a3b: (a: number, b: number, c: any) => void;
-export const wasm_bindgen__closure__destroy__h1e69de07d9687681: (a: number, b: number) => void;
-export const wasm_bindgen__closure__destroy__hb913979280c72850: (a: number, b: number) => void;
-export const wasm_bindgen__convert__closures_____invoke__h668ab64e39a0d10a: (a: number, b: number, c: any, d: any) => void;
-export const wasm_bindgen__convert__closures_____invoke__h7ba3f5c1e6b2bd66: (a: number, b: number) => void;
-export const __wbindgen_malloc_command_export: (a: number, b: number) => number;
-export const __wbindgen_realloc_command_export: (a: number, b: number, c: number, d: number) => number;
-export const __externref_table_alloc_command_export: () => number;
-export const __wbindgen_externrefs: WebAssembly.Table;
-export const __wbindgen_exn_store_command_export: (a: number) => void;
-export const __wbindgen_free_command_export: (a: number, b: number, c: number) => void;
-export const __wbindgen_start: () => void;
+export const __wasm_bindgen_func_elem_67663: (a: number, b: number, c: number) => void;
+export const __wasm_bindgen_func_elem_70889: (a: number, b: number) => void;
+export const __wasm_bindgen_func_elem_70890: (a: number, b: number) => void;
+export const __wasm_bindgen_func_elem_61985: (a: number, b: number, c: number, d: number) => void;
+export const __wasm_bindgen_func_elem_67662: (a: number, b: number) => void;
+export const __wbindgen_export: (a: number, b: number) => number;
+export const __wbindgen_export2: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_export3: (a: number) => void;
+export const __wbindgen_export4: (a: number, b: number, c: number) => void;
