395f3405249ced1efe531fe5036c8a4736d75186
Josh Hale <joshevanhale@gmail.com>
2025-12-13 20:47:24 -0500

    added release build

---
+++ static/game/open_dwarf_lib.d.ts
@@ -0,0 +1,45 @@
+/* tslint:disable */
+/* eslint-disable */
+
+export function main(): void;
+
+export function send_game_bytes(bytes: Uint8Array): void;
+
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+export interface InitOutput {
+  readonly memory: WebAssembly.Memory;
+  readonly main: () => void;
+  readonly send_game_bytes: (a: number, b: number) => void;
+  readonly __wasm_bindgen_func_elem_67683: (a: number, b: number, c: number) => void;
+  readonly __wasm_bindgen_func_elem_70909: (a: number, b: number) => void;
+  readonly __wasm_bindgen_func_elem_67682: (a: number, b: number) => void;
+  readonly __wasm_bindgen_func_elem_62006: (a: number, b: number, c: number, d: number) => void;
+  readonly __wasm_bindgen_func_elem_70910: (a: number, b: number) => void;
+  readonly __wbindgen_export: (a: number, b: number) => number;
+  readonly __wbindgen_export2: (a: number, b: number, c: number, d: number) => number;
+  readonly __wbindgen_export3: (a: number) => void;
+  readonly __wbindgen_export4: (a: number, b: number, c: number) => void;
+}
+
+export type SyncInitInput = BufferSource | WebAssembly.Module;
+
+/**
+* Instantiates the given `module`, which can either be bytes or
+* a precompiled `WebAssembly.Module`.
+*
+* @param {{ module: SyncInitInput }} module - Passing `SyncInitInput` directly is deprecated.
+*
+* @returns {InitOutput}
+*/
+export function initSync(module: { module: SyncInitInput } | SyncInitInput): InitOutput;
+
+/**
+* If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+* for everything else, calls `WebAssembly.instantiate` directly.
+*
+* @param {{ module_or_path: InitInput | Promise<InitInput> }} module_or_path - Passing `InitInput` directly is deprecated.
+*
+* @returns {Promise<InitOutput>}
+*/
+export default function __wbg_init (module_or_path?: { module_or_path: InitInput | Promise<InitInput> } | InitInput | Promise<InitInput>): Promise<InitOutput>;
